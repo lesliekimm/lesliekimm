8a675127d4b3354c0659de440f109aec1d1c938e
leslie kimm <leslie.kimm@gmail.com>
2016-04-27 09:38:51 -0700

    edit views directory structure & udpate routes

--- routes/index.js
+++ routes/index.js
@@ -28,48 +28,48 @@ router.get('/contact', function(req, res, next) {
 
 /*** PORTFOLIOS ***/
 /* GET software portfolio page. */
-router.get('/software', function(req, res, next) {
+router.get('/portfolio/software', function(req, res, next) {
   res.render('portfolio/software', { title: '| software |' });
 });
 
 /* GET art portfolio page. */
-router.get('/art', function(req, res, next) {
+router.get('/portfolio/art', function(req, res, next) {
   res.render('portfolio/art', { title: '| art |' });
 });
 
 /*** ART PORTFOLIOS ***/
 /* GET nothing's impossible portfolio page. */
-router.get('/art/nothings_impossible', function(req, res, next) {
+router.get('/portfolio/art/nothings_impossible', function(req, res, next) {
   res.render('portfolio/art/nothings_impossible', { title: '| nothings impossible |' });
 });
 
 /* GET paintings portfolio page. */
-router.get('/art/paintings', function(req, res, next) {
+router.get('/portfolio/art/paintings', function(req, res, next) {
   res.render('portfolio/art/paintings', { title: '| paintings |' });
 });
 
 /* GET design work portfolio page. */
-router.get('/art/design_work', function(req, res, next) {
+router.get('/portfolio/art/design_work', function(req, res, next) {
   res.render('portfolio/art/design_work', { title: '| design work |' });
 });
 
 /* GET other mediums portfolio page. */
-router.get('/art/other_mediums', function(req, res, next) {
+router.get('/portfolio/art/other_mediums', function(req, res, next) {
   res.render('portfolio/art/other_mediums', { title: '| other mediums |' });
 });
 
 /* GET layout portfolio page. */
-router.get('/art/layout', function(req, res, next) {
+router.get('/portfolio/art/layout', function(req, res, next) {
   res.render('portfolio/art/layout', { title: '| layout |' });
 });
 
 /* GET photography portfolio page. */
-router.get('/art/photography', function(req, res, next) {
+router.get('/portfolio/art/photography', function(req, res, next) {
   res.render('portfolio/art/photography', { title: '| photography |' });
 });
 
 /* GET crafts portfolio page. */
-router.get('/art/crafts', function(req, res, next) {
+router.get('/portfolio/art/crafts', function(req, res, next) {
   res.render('portfolio/art/crafts', { title: '| crafts |' });
 });
 
